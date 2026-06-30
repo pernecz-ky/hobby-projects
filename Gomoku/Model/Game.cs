0c013b161e112a558d8402f5a369b14c9c2ca988
Perneczky Domonkos András <urwehk@inf.elte.hu>
2026-06-30 16:05:30 +0200

    added Board, Piece and Game classes plus the default Program.cs

---
+++ Gomoku/Model/Game.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gomoku.Model
+{
+    public class Game
+    {
+        public Board CurrentBoard{ get; init; }
+        public Piece CurrentPlayer { get; private set; }
+
+        public bool GameOver { get; private set; }
+
+        public Piece Winner { get; private set; }
+
+        public Game(Board cb)
+        {
+            CurrentBoard = cb;
+            CurrentPlayer = Piece.White;
+            GameOver = false;
+            Winner = Piece.Empty; //signifies that no winner has been detected yet
+        }
+
+        public bool TryPlayMove(int x, int y)
+        {
+            if (!GameOver && CurrentBoard.IsValidPlacement(x,y))
+            {
+                CurrentBoard.PlacePiece(x, y, CurrentPlayer);
+                if (CurrentBoard.CheckWinCondition(x, y, CurrentPlayer))
+                {
+                    GameOver = true;
+                    Winner = CurrentPlayer;
+                    return true;
+                }
+                else if (CurrentBoard.IsBoardFull())
+                {
+                    GameOver = true;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void ResetGame()
+        {
+            CurrentBoard.ClearBoard();
+            GameOver = false;
+            CurrentPlayer = Piece.White;
+            Winner = Piece.Empty;
+        }
+    }
+}
---
+++ Gomoku/Model/Piece.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gomoku.Model
+{
+    public enum Piece { Empty, Black, White}
+}
